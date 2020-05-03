cbb39122b6696b6ccae69c627653a4e00cbbdc27
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-05-03 17:47:10 +0300

    screen added

--- HowToHandleToggleInSwiftUI/ContentView.swift
+++ HowToHandleToggleInSwiftUI/ContentView.swift
@@ -19,10 +19,39 @@ class TestStore: ObservableObject {
 
 struct ContentView: View {
     @ObservedObject var store = TestStore()
+    @State private var rotateA = false
+    @State private var rotateB = false
+    
+    let fromLeftToRight = LinearGradient(gradient: Gradient(colors: [.green, .yellow]), startPoint: .leading, endPoint: .trailing)
+    let fromTopToBottom = LinearGradient(gradient: Gradient(colors: [.green, .yellow]), startPoint: .top, endPoint: .bottom)
+    let fromLeftToTopRight = LinearGradient(gradient: Gradient(colors: [.green, .yellow]), startPoint: .leading, endPoint: .topTrailing)
+    let fromRightToTopLeft = LinearGradient(gradient: Gradient(colors: [.green, .yellow]), startPoint: UnitPoint(x: 0.9, y: 0.9), endPoint: UnitPoint(x: 0.25, y: 0.25))
+    
     var body: some View {
-        VStack {
-            Text(store.text)
-            Toggle(isOn: $store.isOn) { Text("On:")}.fixedSize()
+        ZStack {
+            Rectangle()
+                .fill(self.store.isOn ?
+                    (self.rotateA && self.rotateB ? fromLeftToRight : fromRightToTopLeft) :
+                    (self.rotateA && self.rotateB ? fromTopToBottom : fromLeftToTopRight))
+                .edgesIgnoringSafeArea(.all)
+                .animation(.default)
+            
+            VStack {
+                Text(store.text)
+                Toggle(isOn: $store.isOn) {
+                    Text("On:")
+                }.fixedSize()
+                Button(action: {
+                    self.rotateA.toggle()
+                    self.rotateB.toggle()
+                }) {
+                    Text("Rotate")
+                        .bold()
+                }
+            }
+        }.onTapGesture {
+            self.rotateA.toggle()
+            self.store.isOn.toggle()
         }
     }
 }
